6ddc96a4cbd41e69879314f91728aa08bb4de36a
Roman Ishchuk <01165722@pw.edu.pl>
2026-07-20 21:43:35 +0200

    Update sql CTE with additional tasks

--- queries/05_cte.sql
+++ queries/05_cte.sql
@@ -175,3 +175,210 @@ WITH Monthly_sales (year, month, revenue) AS (
 SELECT year, month, revenue 
 FROM Monthly_sales
 ORDER BY year, month;
+
+
+-- Task 11
+-- Find the month with the highest revenue
+
+WITH Monthly_sales (year, month, revenue) AS (
+	SELECT 
+		YEAR(order_date),
+        MONTH(order_date),
+        SUM(total_amount)
+	FROM Orders
+    GROUP BY YEAR(order_date), MONTH(order_date)
+)
+SELECT year, month, revenue 
+FROM Monthly_sales
+ORDER BY revenue DESC
+LIMIT 1;
+
+
+-- Task 12
+-- Create an order summary.
+-- Display: order id, customer, number of products, order total
+
+WITH Order_summary (order_id, customer, number_of_products, order_total) AS (
+	SELECT 
+		o.id, 
+        CONCAT(c.first_name, ' ', c.last_name),
+        SUM(oi.quantity),
+        o.total_amount
+	FROM Orders o
+    JOIN Customers c
+		ON c.id=o.customer_id
+	JOIN Order_Items oi
+		ON o.id=oi.order_id
+	GROUP BY o.id, c.first_name,
+        c.last_name, o.total_amount
+)
+SELECT 
+	order_id, customer, 
+    number_of_products, order_total
+FROM Order_summary;
+
+
+-- Task 13
+-- Calculate the average product price for every category.
+-- Then show only categories above the global average
+
+WITH Avg_prod_pirce_by_category (category, avg_price) AS (
+	SELECT 
+		c.name,
+        AVG(p.price)
+	FROM Products p
+    JOIN Categories c
+		ON c.id=p.category_id
+	GROUP BY c.id, c.name
+)
+SELECT category, avg_price
+FROM Avg_prod_pirce_by_category
+WHERE avg_price>(
+	SELECT AVG(avg_price)
+    FROM Avg_prod_pirce_by_category
+);
+
+
+-- Task 14
+-- Create a customer statistics report.
+-- Display: customer, orders, total spent, average order value
+
+WITH Statistics_report (customer, orders, total_spent, average_order_value) AS (
+	SELECT
+		CONCAT(c.first_name, ' ', c.last_name),
+		COUNT(o.id),
+        SUM(o.total_amount),
+        AVG(o.total_amount)
+	FROM Orders o
+    JOIN Customers c
+		ON c.id=o.customer_id
+	GROUP BY c.id, c.first_name, c.last_name
+)
+SELECT 
+	customer, orders, total_spent, average_order_value 
+FROM Statistics_report;
+
+
+-- Task 15
+-- Find customers whose average order value is above the average order value of all customers
+
+WITH Customers_average (customer, average_order_value) AS (
+	SELECT
+		CONCAT(c.first_name, ' ', c.last_name), 
+        AVG(o.total_amount)
+	FROM Orders o
+    JOIN Customers c
+		ON c.id=o.customer_id
+	GROUP BY c.id, c.first_name, c.last_name
+)
+SELECT customer, average_order_value
+FROM Customers_average
+WHERE average_order_value>(
+	SELECT AVG(average_order_value)
+    FROM Customers_average
+);
+
+
+-- Task 16
+-- Find products that generated no revenue
+
+WITH Product_with_no_revenue (product, revenue) AS (
+	SELECT 
+		p.name,
+        COALESCE(SUM(oi.quantity * oi.unit_price), 0)
+	FROM Products p
+    LEFT JOIN Order_Items oi
+		ON p.id=oi.product_id
+	 GROUP BY p.id, p.name
+)
+SELECT product
+FROM Product_with_no_revenue
+WHERE revenue=0;
+
+
+-- Task 17
+-- Create a category performance report.
+-- Display: category, products, revenue, average price, stock quantity
+
+WITH Performance_report (category, products, revenue, average_price, stock_quantity) AS (
+	SELECT
+		c.name,
+        COUNT(DISTINCT p.id),
+        COALESCE(SUM(oi.quantity*oi.unit_price), 0),
+        AVG(p.price),
+        SUM(p.stock_quantity)
+	FROM Categories c
+    JOIN Products p
+		ON c.id=p.category_id
+	LEFT JOIN Order_Items oi
+		ON p.id=oi.product_id
+	GROUP BY c.id, c.name
+)
+SELECT 
+	category, products, revenue, average_price, stock_quantity
+FROM Performance_report;
+
+
+-- Task 18
+-- Find the top 3 customers by spending
+
+WITH Customers_spent (customer, total_amount) AS (
+	SELECT
+		CONCAT(c.first_name, ' ', c.last_name),
+        SUM(o.total_amount)
+	FROM Orders o
+    JOIN Customers c
+		ON c.id=o.customer_id
+	GROUP BY c.id, c.first_name, c.last_name
+)
+SELECT customer, total_amount
+FROM Customers_spent
+ORDER BY total_amount DESC
+LIMIT 3;
+
+
+-- Task 19
+-- Create a sales dashboard.
+-- Display: total customers, total orders, total products, total revenue
+
+WITH All_customers AS (
+	SELECT COUNT(*) AS total_customers
+    FROM Customers
+), 
+All_orders AS (
+	SELECT COUNT(*) AS total_orders,
+		SUM(total_amount) AS total_revenue
+    FROM Orders
+),
+All_products AS (
+	SELECT COUNT(*) AS total_products
+    FROM Products
+)
+SELECT 
+	c.total_customers, o.total_orders, p.total_products, o.total_revenue
+FROM All_orders o 
+CROSS JOIN All_customers c
+CROSS JOIN All_products p;
+
+
+-- Task 20
+-- Create a complete business report
+-- Display:
+-- | Customer | Orders | Total Spent | Avg Order |
+-- Prepare the data so that it can later be extended with window functions
+
+WITH Business_report (customer, orders, total_spent, avg_order) AS (
+	SELECT 
+		CONCAT(c.first_name, ' ', c.last_name),
+        COUNT(o.id),
+        SUM(o.total_amount),
+        AVG(o.total_amount)
+	FROM Customers c
+	JOIN Orders o
+		ON c.id=o.customer_id
+	GROUP BY c.id, c.first_name, c.last_name
+)
+SELECT 
+	customer, orders, total_spent, avg_order
+FROM Business_report
+ORDER BY customer;
