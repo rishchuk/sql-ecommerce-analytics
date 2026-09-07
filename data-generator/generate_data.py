befaf04ba284629ef0b28bf33249bcd2e81950f3
Roman Ishchuk <i.roman.zp@gmail.com>
2026-09-07 19:27:42 +0200

    feat: generate categories

--- data-generator/generate_data.py
+++ data-generator/generate_data.py
@@ -1,7 +1,10 @@
 import os
 
+import random
+from faker import Faker
 from dotenv import load_dotenv
 from sqlalchemy import create_engine, text
+
 
 load_dotenv()
 
@@ -12,6 +15,46 @@ if not DATABASE_URL:
 
 engine = create_engine(DATABASE_URL)
 
-with engine.connect() as connection:
-    result = connection.execute(text("SELECT 1"))
-    print(result.scalar())
+CUSTOMERS_COUNT = 5_000
+PRODUCTS_COUNT = 500
+ORDERS_COUNT = 50_000
+RANDOM_SEED = 42
+
+
+fake = Faker()
+
+random.seed(RANDOM_SEED)
+Faker.seed(RANDOM_SEED)
+
+
+CATEGORIES = [
+    "Laptops",
+    "Smartphones",
+    "Tablets",
+    "Monitors",
+    "Accessories",
+    "Gaming",
+    "Networking",
+    "Storage",
+    "Audio",
+    "Office",
+]
+
+def generate_categories():
+    rows = []
+
+    for name in CATEGORIES:
+        rows.append({
+            "name": name
+        })
+
+    with engine.begin() as connection:
+        connection.execute(
+            text("""
+                INSERT INTO Categories (name)
+                VALUES (:name)
+            """),
+            rows
+        )
+
+generate_categories()
